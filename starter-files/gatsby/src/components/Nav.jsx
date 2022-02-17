87869097add1e4260813d7d7f44318ab03260403
Yoni Touboul <yonitouboul@hotmail.com>
2022-02-17 22:01:49 +0100

    REsponsive

--- starter-files/gatsby/src/components/Nav.jsx
+++ starter-files/gatsby/src/components/Nav.jsx
@@ -10,11 +10,11 @@ const StyledNav = styled.nav`
   ul {
     margin: 0;
     padding: 0;
+    text-align: center;
+    list-style: none;
     display: grid;
     grid-template-columns: 1fr 1fr auto 1fr 1fr;
     grid-gap: 2rem;
-    text-align: center;
-    list-style: none;
     align-items: center;
     margin-top: -6rem;
   }
@@ -38,12 +38,37 @@ const StyledNav = styled.nav`
   a {
     font-size: 3rem;
     text-decoration: none;
+    display: block;
     &:hover {
       color: var(--red);
     }
-    &[aria-current='page'] {
-      color: var(--red);
+    @media (max-width: 800px) {
+      font-size: 2rem;
     }
+    /* &[aria-current='page'] {
+      color: var(--red);
+    } */
+  }
+  @media (max-width: 600px) {
+    --columns: 4;
+    margin-bottom: 2rem;
+    border-bottom: 2px solid var(--grey);
+    padding-bottom: 2rem;
+    ul {
+      grid-template-rows: auto auto;
+      grid-template-columns: repeat(var(--columns), 1fr);
+      justify-items: center;
+    }
+    .logo-item {
+      order: 0;
+      grid-column: 1 / -1;
+    }
+    .logo {
+      transform: none;
+    }
+  }
+  @media (max-width: 500px) {
+    --columns: 2;
   }
 `;
 
